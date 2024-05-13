44ec6c8ecb8c1b22b925061320fa283c21d1f546
정유진 <yujin3504@gmail.com>
2024-05-13 19:59:59 +0900

    [Design] 시간 컴포넌트 추가

--- WakeUpClock/Controllers/StopwatchViewController.swift
+++ WakeUpClock/Controllers/StopwatchViewController.swift
@@ -10,27 +10,128 @@ import SnapKit
 
 class StopwatchViewController: UIViewController {
     
-    let testLabel: UILabel = {
+    // MARK: - 컴포넌트 추가
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.alignment = .center
+        stackView.distribution = .fill
+        stackView.spacing = 20
+        return stackView
+    }()
+    
+    private let hoursLabel: UILabel = {
         let label = UILabel()
-        label.text = "test"
+        label.text = "00"
         label.textColor = .white
-        label.font = UIFont.systemFont(ofSize: 16)
+        label.font = UIFont.systemFont(ofSize: 32, weight: .semibold)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
+    private let hoursSeparatorLabel: UILabel = {
+        let label = UILabel()
+        label.text = ":"
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 32, weight: .semibold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let minutesLabel: UILabel = {
+        let label = UILabel()
+        label.text = "00"
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 32, weight: .semibold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let minutesSeparatorLabel: UILabel = {
+        let label = UILabel()
+        label.text = ":"
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 32, weight: .semibold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let secondsLabel: UILabel = {
+        let label = UILabel()
+        label.text = "00"
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 32, weight: .semibold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    // MARK: - viewDidLoad()
     override func viewDidLoad() {
         super.viewDidLoad()
         setupConstraints()
     }
     
-    func setupConstraints() {
-        view.addSubview(testLabel)
+    // MARK: - 레이아웃 설정
+    private func setupConstraints() {
+        view.addSubview(stackView)
         
-        testLabel.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide).inset(20)
-            $0.leading.equalToSuperview().inset(24)
+        let hourView = createNumberContainerView()
+        hourView.addSubview(hoursLabel)
+        
+        let minuteView = createNumberContainerView()
+        minuteView.addSubview(minutesLabel)
+        
+        let secondView = createNumberContainerView()
+        secondView.addSubview(secondsLabel)
+        
+        [hourView, hoursSeparatorLabel, minuteView, minutesSeparatorLabel, secondView].forEach {
+            stackView.addArrangedSubview($0)
         }
+        
+        stackView.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(216)
+        }
+        
+        hourView.snp.makeConstraints {
+            $0.width.equalTo(52)
+            $0.height.equalTo(56)
+        }
+        
+        hoursLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalToSuperview()
+        }
+        
+        minuteView.snp.makeConstraints {
+            $0.width.equalTo(52)
+            $0.height.equalTo(56)
+        }
+        
+        minutesLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalToSuperview()
+        }
+        
+        secondView.snp.makeConstraints {
+            $0.width.equalTo(52)
+            $0.height.equalTo(56)
+        }
+        
+        secondsLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalToSuperview()
+        }
+        
     }
     
+    private func createNumberContainerView() -> UIView {
+        let containerView = UIView()
+        containerView.backgroundColor = UIColor(red: 153/255, green: 153/255, blue: 200/255, alpha: 0.1)
+        containerView.layer.cornerRadius = 8
+        return containerView
+    }
+}
+
+#Preview {
+    StopwatchViewController()
 }
