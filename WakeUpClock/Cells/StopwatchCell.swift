312131411a2e8786a091dea8d0ce1fee1d75acbb
정유진 <yujin3504@gmail.com>
2024-05-14 15:22:00 +0900

    [Design] 랩 테이블뷰 구성

--- WakeUpClock/Cells/StopwatchCell.swift
+++ WakeUpClock/Cells/StopwatchCell.swift
@@ -6,18 +6,68 @@
 //
 
 import UIKit
+import SnapKit
 
 class StopwatchCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    static let identifier = "StopwatchCell"
+    
+    let lapLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 17)
+        label.textColor = UIColor(named: "textColor")
+        return label
+    }()
+    
+    let recordLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textColor = UIColor(named: "textColor")
+        return label
+    }()
+    
+    let diffLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textColor = UIColor(named: "textColor")
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureUI()
+        setupConstraints()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupConstraints() {
+        contentView.addSubview(lapLabel)
+        contentView.addSubview(recordLabel)
+        contentView.addSubview(diffLabel)
+        
+        lapLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().offset(16) 
+        }
+        
+        recordLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.centerX.equalToSuperview()
+        }
+        
+        diffLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(16)
+        }
+        
+    }
+    
+    private func configureUI() {
+        contentView.backgroundColor = UIColor(named: "backGroudColor")
+        selectionStyle = .none
     }
 
 }
--- WakeUpClock/Controllers/StopwatchViewController.swift
+++ WakeUpClock/Controllers/StopwatchViewController.swift
@@ -10,7 +10,12 @@ import SnapKit
 
 class StopwatchViewController: UIViewController {
     
-    // MARK: - 컴포넌트 추가
+    // MARK: - 프로퍼티
+    private let mainStopwatch: Stopwatch = Stopwatch()
+    private var isPlay: Bool = false
+    private var lapTableViewData: [String] = []
+    
+    // MARK: - 컴포넌트
     private let stackView: UIStackView = {
         let stackView = UIStackView()
         stackView.alignment = .center
@@ -98,12 +103,21 @@ class StopwatchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupConstraints()
+        configureUI()
+    }
+    
+    private func configureUI() {
+        lapButton.isEnabled = false
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(StopwatchCell.self, forCellReuseIdentifier: StopwatchCell.identifier)
+        tableView.backgroundColor = UIColor(named: "backGroudColor")
     }
     
     // MARK: - 레이아웃 설정
     private func setupConstraints() {
         
-        [stackView, lapButton, startButton].forEach {
+        [stackView, lapButton, startButton, tableView].forEach {
             view.addSubview($0)
         }
         
@@ -169,6 +183,11 @@ class StopwatchViewController: UIViewController {
             $0.trailing.equalToSuperview().inset(32)
         }
         
+        tableView.snp.makeConstraints {
+            $0.top.equalTo(startButton.snp.bottom).offset(40)
+            $0.trailing.leading.equalToSuperview()
+            $0.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
     }
     
     private func createNumberContainerView() -> UIView {
@@ -177,4 +196,20 @@ class StopwatchViewController: UIViewController {
         containerView.layer.cornerRadius = 8
         return containerView
     }
+}
+extension StopwatchViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: StopwatchCell.identifier, for: indexPath) as? StopwatchCell else { return UITableViewCell() }
+        cell.lapLabel.text = "lap"
+        cell.recordLabel.text = "record"
+        cell.diffLabel.text = "diff"
+        return cell
+    }
+}
+#Preview{
+    StopwatchViewController()
 }
