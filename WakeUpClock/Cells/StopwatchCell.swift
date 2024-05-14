eb658e9251778ed9e70e0926c96adcb2b9c6500d
정유진 <yujin3504@gmail.com>
2024-05-14 14:57:44 +0900

    [Feat] Stopwatch 클래스 구현

---
+++ WakeUpClock/Cells/StopwatchCell.swift
@@ -0,0 +1,23 @@
+//
+//  StopwatchCell.swift
+//  WakeUpClock
+//
+//  Created by 정유진 on 5/14/24.
+//
+
+import UIKit
+
+class StopwatchCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ WakeUpClock/Models/Stopwatch.swift
@@ -0,0 +1,18 @@
+//
+//  Stopwatch.swift
+//  WakeUpClock
+//
+//  Created by 정유진 on 5/14/24.
+//
+
+import Foundation
+
+class Stopwatch: NSObject {
+  var counter: Double
+  var timer: Timer
+  
+  override init() {
+    counter = 0.0
+    timer = Timer()
+  }
+}
