39496aa9e08b1a604c6774bf1a2c35fadf1afbad
정유진 <yujin3504@gmail.com>
2024-05-14 20:08:53 +0900

    [Feat] 타이머 업데이트 구현

--- WakeUpClock/Controllers/StopwatchViewController.swift
+++ WakeUpClock/Controllers/StopwatchViewController.swift
@@ -12,6 +12,7 @@ class StopwatchViewController: UIViewController {
     
     // MARK: - 프로퍼티
     private let mainStopwatch: Stopwatch = Stopwatch()
+    private let lapStopwatch: Stopwatch = Stopwatch()
     private var isPlay: Bool = false
     private var lapTableViewData: [String] = []
     
@@ -22,24 +23,6 @@ class StopwatchViewController: UIViewController {
         stackView.distribution = .fill
         stackView.spacing = 20
         return stackView
-    }()
-    
-    private let hoursLabel: UILabel = {
-        let label = UILabel()
-        label.text = "00"
-        label.textColor = .white
-        label.font = UIFont.systemFont(ofSize: 32, weight: .semibold)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
-    private let hoursSeparatorLabel: UILabel = {
-        let label = UILabel()
-        label.text = ":"
-        label.textColor = .white
-        label.font = UIFont.systemFont(ofSize: 32, weight: .semibold)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
     }()
     
     private let minutesLabel: UILabel = {
@@ -69,14 +52,32 @@ class StopwatchViewController: UIViewController {
         return label
     }()
     
+    private let secondsSeparatorLabel: UILabel = {
+        let label = UILabel()
+        label.text = ":"
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 32, weight: .semibold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let milliSecondsLabel: UILabel = {
+        let label = UILabel()
+        label.text = "00"
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 32, weight: .semibold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     private let lapButton: UIButton = {
         let button = UIButton()
         button.setTitle("Lap", for: .normal)
-        button.setTitleColor(UIColor(named: "mainInactiveTextColor"), for: .normal)
+        button.setTitleColor(UIColor.gray, for: .normal)
         button.backgroundColor = .clear
         button.layer.cornerRadius = 24
         button.layer.borderWidth = 1
-        button.layer.borderColor = UIColor(named: "mainInactiveTextColor")?.cgColor
+        button.layer.borderColor = UIColor.gray.cgColor
         return button
     }()
     
@@ -85,7 +86,6 @@ class StopwatchViewController: UIViewController {
         let button = UIButton()
         button.setTitle("Start", for: .normal)
         button.setTitleColor(UIColor(named: "mainActiveColor"), for: .normal)
-        button.backgroundColor = .gray
         button.backgroundColor = .clear
         button.layer.cornerRadius = 24
         button.layer.borderWidth = 1
@@ -99,11 +99,12 @@ class StopwatchViewController: UIViewController {
         return tableView
     }()
     
-    // MARK: - viewDidLoad()
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setupConstraints()
         configureUI()
+        setupButtons()
     }
     
     private func configureUI() {
@@ -121,32 +122,22 @@ class StopwatchViewController: UIViewController {
             view.addSubview($0)
         }
         
-        let hourView = createNumberContainerView()
-        hourView.addSubview(hoursLabel)
-        
         let minuteView = createNumberContainerView()
         minuteView.addSubview(minutesLabel)
         
         let secondView = createNumberContainerView()
         secondView.addSubview(secondsLabel)
         
-        [hourView, hoursSeparatorLabel, minuteView, minutesSeparatorLabel, secondView].forEach {
+        let msView = createNumberContainerView()
+        msView.addSubview(milliSecondsLabel)
+        
+        [minuteView, minutesSeparatorLabel, secondView, secondsSeparatorLabel, msView].forEach {
             stackView.addArrangedSubview($0)
         }
         
         stackView.snp.makeConstraints {
             $0.centerX.equalToSuperview()
             $0.top.equalTo(view.safeAreaLayoutGuide).offset(216)
-        }
-        
-        hourView.snp.makeConstraints {
-            $0.width.equalTo(52)
-            $0.height.equalTo(56)
-        }
-        
-        hoursLabel.snp.makeConstraints {
-            $0.centerX.equalToSuperview()
-            $0.centerY.equalToSuperview()
         }
         
         minuteView.snp.makeConstraints {
@@ -165,6 +156,16 @@ class StopwatchViewController: UIViewController {
         }
         
         secondsLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalToSuperview()
+        }
+        
+        msView.snp.makeConstraints {
+            $0.width.equalTo(52)
+            $0.height.equalTo(56)
+        }
+        
+        milliSecondsLabel.snp.makeConstraints {
             $0.centerX.equalToSuperview()
             $0.centerY.equalToSuperview()
         }
@@ -198,35 +199,73 @@ class StopwatchViewController: UIViewController {
     }
     
     // MARK: - 버튼 이벤트 처리
-    
     private func setupButtons() {
         lapButton.addTarget(self, action: #selector(lapButtonPressed), for: .touchUpInside)
+        startButton.addTarget(self, action: #selector(startButtonPressed), for: .touchUpInside)
     }
     
     @objc private func lapButtonPressed() {
-       
-        // 시간이 멈춰있을 때 -> 버튼 누르면 lap으로 바뀌어야 함
+        
+        // 시간이 멈춰있을 때 -> 버튼 누르면 reset 되어야 함
         if !isPlay {
             resetMainTimer()
-            changeButton(lapButton, title: "Lap", titleColor: UIColor(named: "mainInactiveTextColor") ?? UIColor(red: 174/255, green: 174/255, blue: 183/255, alpha: 1.0))
+            changeButton(lapButton, title: "Lap", titleColor: UIColor.gray)
+            lapButton.layer.borderColor = UIColor.gray.cgColor
             lapButton.isEnabled = false
         }
         
         // 시간이 가고 있을 때 -> 테이블 뷰 셀의 데이터를 추가
         else {
-            let timerLabelText = "\(hoursLabel.text ?? "00"):\(minutesLabel.text ?? "00"):\(secondsLabel.text ?? "00")"
+            let timerLabelText = "\(minutesLabel.text ?? "00"):\(secondsLabel.text ?? "00"):\(milliSecondsLabel.text ?? "00")"
             lapTableViewData.append(timerLabelText)
         }
         
         tableView.reloadData()
     }
+    
+    @objc private func startButtonPressed() {
+        lapButton.isEnabled = true
+        
+        lapButton.setTitle("Lap", for: .normal)
+        lapButton.setTitleColor(UIColor(named: "mainTextColor"), for: .normal)
+        lapButton.layer.borderColor = UIColor(named: "mainTextColor")?.cgColor
+        
+        // 시간이 멈춰있을 때 -> 버튼 누르면 시간이 흘러야 함
+        if !isPlay {
+            unowned let weakSelf = self
+            mainStopwatch.timer = Timer.scheduledTimer(timeInterval: 0.01, target: weakSelf, selector: Selector.updateMainTimer, userInfo: nil, repeats: true)
+            
+            isPlay = true
+            changeButton(startButton, title: "Stop", titleColor: UIColor.red)
+            startButton.layer.borderColor = UIColor.red.cgColor
+        }
+        
+        // 시간이 흐를 때 -> 버튼 누르면 멈춰야 함
+        else {
+            mainStopwatch.timer.invalidate()
+            lapStopwatch.timer.invalidate()
+            
+            isPlay = false
+            
+            startButton.setTitle("Start", for: .normal)
+            startButton.setTitleColor(UIColor(named: "mainActiveColor"), for: .normal)
+            startButton.layer.borderColor = UIColor(named: "mainActiveColor")?.cgColor
+ 
+            lapButton.setTitle("Reset", for: .normal)
+            lapButton.setTitleColor(UIColor(named: "mainTextColor"), for: .normal)
+            lapButton.layer.borderColor = UIColor(named: "mainTextColor")?.cgColor
+        }
+        
+    }
 }
+
 // MARK: - Action Functions
 extension StopwatchViewController {
+    
     func changeButton(_ button: UIButton, title: String, titleColor: UIColor) {
         button.setTitle(title, for: UIControl.State())
         button.setTitleColor(titleColor, for: .normal)
-    }
+    } // --> 수정해야함!
     
     func resetTimer(_ stopwatch: Stopwatch, labels: [UILabel]) {
         stopwatch.timer.invalidate()
@@ -237,13 +276,31 @@ extension StopwatchViewController {
     }
     
     func resetMainTimer() {
-        resetTimer(mainStopwatch, labels: [hoursLabel, minutesLabel, secondsLabel])
+        resetTimer(mainStopwatch, labels: [minutesLabel, secondsLabel, milliSecondsLabel])
         lapTableViewData.removeAll()
         tableView.reloadData()
     }
+    
+    @objc func updateMainTimer() {
+        updateTimer(mainStopwatch, labels: [minutesLabel, secondsLabel, milliSecondsLabel])
+    }
+    
+    func updateTimer(_ stopwatch: Stopwatch, labels: [UILabel]) {
+        stopwatch.counter += 0.01
+        
+        let minutes = Int(stopwatch.counter / 60)
+        let seconds = Int(stopwatch.counter.truncatingRemainder(dividingBy: 60))
+        let milliseconds = Int((stopwatch.counter * 100).truncatingRemainder(dividingBy: 100))
+        
+        labels[0].text = String(format: "%02d", minutes) // 분
+        labels[1].text = String(format: "%02d", seconds) // 초
+        labels[2].text = String(format: "%02d", milliseconds) // 밀리초
+    }
 }
 
-
+fileprivate extension Selector {
+    static let updateMainTimer = #selector(StopwatchViewController.updateMainTimer)
+}
 
 // MARK: - TableView Extenseion
 extension StopwatchViewController: UITableViewDelegate, UITableViewDataSource {
