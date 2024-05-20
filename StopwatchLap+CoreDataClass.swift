28aa96153595378acc42b2052fb96a71f6f7dc51
정유진 <yujin3504@gmail.com>
2024-05-20 17:11:11 +0900

    [Feat] add coredata lap model

---
+++ StopwatchLap+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  StopwatchLap+CoreDataClass.swift
+//  WakeUpClock
+//
+//  Created by 정유진 on 5/20/24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(StopwatchLap)
+public class StopwatchLap: NSManagedObject {
+
+}
---
+++ StopwatchLap+CoreDataProperties.swift
@@ -0,0 +1,27 @@
+//
+//  StopwatchLap+CoreDataProperties.swift
+//  WakeUpClock
+//
+//  Created by 정유진 on 5/20/24.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension StopwatchLap {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<StopwatchLap> {
+        return NSFetchRequest<StopwatchLap>(entityName: "StopwatchLap")
+    }
+
+    @NSManaged public var lapNumber: Int64
+    @NSManaged public var recordTime: String?
+    @NSManaged public var diffTime: String?
+
+}
+
+extension StopwatchLap : Identifiable {
+
+}
